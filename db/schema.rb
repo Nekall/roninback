c71034fcba9f0b9c1127245a1bfbafac957d0e2d
Neka <nekoanchor@yahoo.fr>
2021-06-15 11:52:57 +0200

    feat: add ressources table & joinTable with user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_223356) do
+ActiveRecord::Schema.define(version: 2021_06_15_094817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 2021_06_14_223356) do
     t.index ["sender_id"], name: "index_privatemessagings_on_sender_id"
   end
 
+  create_table "ressources", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_ressources_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -74,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_06_14_223356) do
     t.index ["user_id"], name: "index_users_badges_on_user_id"
   end
 
+  add_foreign_key "ressources", "users"
   add_foreign_key "users_badges", "badges"
   add_foreign_key "users_badges", "users"
 end
