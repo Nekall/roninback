ac6de80b1506af1979ae598fd5c3e065bea7fb77
Neka <nekoanchor@yahoo.fr>
2021-06-16 16:26:44 +0200

    fix: turn off env mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
 
   # On/Off letter Opener
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
