f8ec3701081dd9d0862ddef77d87ed4a1a7c5333
Neka <nekoanchor@yahoo.fr>
2021-06-16 16:08:51 +0200

    fix: function welcome send

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,8 @@ class UserMailer < ApplicationMailer
   default from: 'no-reply@ronincode.fr'
 
   def welcome_email(user)
-    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user
-    #on définit une variable @url qu'on utilisera dans la view d'e-mail
     @url = 'http://ronincode.fr/login'
-    # c'est cet appel à mail() qui permet d'envoyer l'e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez 浪漫コード RoninCode !')
   end
 
