901fbad5ee903e8cd5db703ba2de7fceef0c2dee
Neka <nekoanchor@yahoo.fr>
2021-06-16 13:04:39 +0200

    fix: escape slash

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,6 @@ class Message < ApplicationRecord
   validates_presence_of :body, :privatemessaging_id, :user_id
 
   def message_time
-    created_at.strftime(“%m\/%d\/%y at %l:%M %p”)
+    created_at.strftime(`%m/%d/%y at %l:%M %p`)
   end
 end
