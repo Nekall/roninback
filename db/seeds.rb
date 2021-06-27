eaae11ca5c9d2894b52c14b96a7fea86536f4497
Tex <julestexier@live.fr>
2021-06-27 15:37:55 +0200

    feature: include technologies on profil

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,13 +19,13 @@ User.create(
   is_admin: true
 )
 
-10.times do
+50.times do
   User.create(
     username: Faker::Internet.username,
     firstname: Faker::Name.first_name,
     lastname: Faker::Name.last_name,
     email: Faker::Internet.email,
-    password: "password"
+    password: "password",
   )
 end
 
