3b70085f25cdeff1a8c88eb3e7c91b782a386a19
Neka <nekoanchor@yahoo.fr>
2021-07-07 19:57:31 +0200

    fix: wrong url in mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,13 +5,13 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url = 'http://localhost:3000/se-connecter'
+    @url = 'https://ronincode.vercel.app/se-connecter'
     mail(to: @user.email, subject: 'Bienvenue chez RoninCode !')
   end
 
   def forgot_password_email(user)
     @user = user
-    @url  = 'http://localhost:3000/nouveau/mot-de-passe/'
+    @url  = 'https://ronincode.vercel.app/nouveau/mot-de-passe/'
     mail(to: @user.email, subject: 'Réinitialisation du mot de passe pour RoninCode')
   end
 
